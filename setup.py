361d7895ef26492b618907b69eabebdbf5a76a26
Chao Liu <yijinliu@gmail.com>
2019-12-13 12:18:37 -0800

    Add websockets as dependency.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="camect-py",
-    version="0.1.6",
+    version="0.1.7",
     author="Chao Liu",
     author_email="chao@camect.com",
     description="A client library to talk to Camect.",
@@ -15,6 +15,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "websockets",
     ],
     python_requires='>=3.6',
 )
